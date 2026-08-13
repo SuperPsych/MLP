956b96d74818de88b9060f061c4c27ec3c5a9c29
Brendan Malaugh <superpsych10@gmail.com>
2026-08-12 23:18:21 -0400

    97%

--- main.cpp
+++ main.cpp
@@ -5,20 +5,18 @@ int main() {
 
     int input_dim = 784;
     int output_dim = 10;
-    int hidden_dim = 64;
-    int hidden_num = 2;
+    int hidden_dim = 128;
+    int hidden_num = 1;
 
     auto model = Model::initialize(input_dim, output_dim, hidden_dim, hidden_num);
 
     auto train_samples = load_samples("./mnist_train.txt", input_dim, output_dim);
+    auto test_samples = load_samples("./mnist_test.txt", input_dim, output_dim);
 
     int epochs = 50;
-    int batch_size = 64;
+    int batch_size = 32;
     double lr = 0.01;
-    model.train(train_samples, epochs, batch_size, lr);
+    model.train(train_samples, test_samples, epochs, batch_size, lr);
 
-    auto test_samples = load_samples("./mnist_test.txt", input_dim, output_dim);
     model.eval(test_samples);
-
-    Matrix::print(model.predict(test_samples[0].x));
 }
